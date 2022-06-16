4e7e74b8e20dc8330e823e221a8809292ed0948b
fran <fran.josipovic55@gmail.com>
2022-06-16 13:10:43 +0200

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/',(req,res)=>{
   res.send({mssg:"server start"})
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
