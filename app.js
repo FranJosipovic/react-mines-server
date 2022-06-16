99fd1054c88395bc4fe802ce2983b06486d14e33
fran <fran.josipovic55@gmail.com>
2022-06-16 13:52:23 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
 const mongoose = require('mongoose')
-require('dotenv').config()
-const Item = mongoose.model("Item")
 
+require('dotenv').config()
+ 
 mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser:true,
   useUnifiedTopology:true,
@@ -19,52 +18,9 @@ mongoose.connection.on('error',(err)=>{
 
 require('./modals/leaderboardModal.js')
 
+app.use(cors());
 app.use(express.json())
 app.use(require('./routes/leaderboard'))
-app.get('/getLeaderboard/:sortType',(req,res) => {
-  const sortType = req.params
-  if(sortType.sortType === 'default'){
-      Item.find().sort({email : 1}).then(
-          result => {
-              return res.status(200).json({result})
-          }
-      ).catch(err=>{
-          console.log(err);
-      })
-  }else if(sortType.sortType === 'time'){
-      Item.find().sort({"timePlayed.forSort" : 1}).then(
-          result => {
-              return res.status(200).json({result})
-          }
-      ).catch(err=>{
-          console.log(err);
-      })
-  }else if(sortType.sortType === 'dificulty'){
-      Item.find().sort({dificulty : -1}).then(
-          result => {
-              return res.status(200).json({result})
-          }
-      ).catch(err=>{
-          console.log(err);
-      })
-  }else if(sortType.sortType === 'date'){
-      Item.find().sort({createdAt : -1}).then(
-          result => {
-              return res.status(200).json({result})
-          }
-      ).catch(err=>{
-          console.log(err);
-      })
-  }else if(sortType.sortType === 'jokersUsed'){
-      Item.find().sort({jokersUsed : 1}).then(
-          result => {
-              return res.status(200).json({result})
-          }
-      ).catch(err=>{
-          console.log(err);
-      })
-  }
-})
 
 const PORT = process.env.PORT || 8000
 
